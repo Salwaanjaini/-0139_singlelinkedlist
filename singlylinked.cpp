1bf719321c1b6c3f66a5ea6dbcc1fb3f020f1175
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-08 15:29:13 +0700

    duplikasi tidak diizinkan

--- singlylinked.cpp
+++ singlylinked.cpp
@@ -39,6 +39,22 @@ public:
             nodeBaru->next = START;
             START = nodeBaru;
             return;
-        };
+        }
+
+        Node *previous = START;
+        Node *current = START;
+        
+        while ((current != NULL) && (nim<= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+
     }
 };
